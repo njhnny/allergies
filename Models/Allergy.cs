37ffa0887305bb4d5aa8f30993548798e6f9b5f9
Nick Hennessy <njhnny@gmail.com>
2021-04-14 20:25:07 -0700

    add code
    Co-authored-by: Collin Chapman <cchap14@gmail.com>

---
+++ Models/Allergy.cs
@@ -0,0 +1,7 @@
+namespace Allergies.Models
+{
+  public class Allergy
+  {
+    Dictionary<int, string> myDictionary = new Dictionary<int, string>() { {1, "eggs"}, {2, "peanuts"} };
+  }
+}
---
+++ Models/ScoreBreaker.cs
@@ -0,0 +1,20 @@
+namespace Allergies.Models
+{
+  public class someFunction {
+        int [] tempList;
+        for (int i =0; i<validScore.length; i++) {
+        if (enteredScore > 255) {
+            print an error message;
+        }
+        else {
+            if (validScore[i] == enteredScore) {
+            go to dictionary and find value corresponding to key "validScore[i]" and push single element to a list;
+            }
+            else (validScore[i] < enteredScore) {
+                iterate thru validScore and find all entries less than enteredScore push each element to a list;
+            }
+            outputList = AllergyFunction(list)
+            for each element in outpuList print out the values.
+        }
+    }
+}
